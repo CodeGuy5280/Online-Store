8be34636c211261ddc82b6b08b1f8f465d338b89
Izel Ozhan <izelozhan@gmail.com>
2025-04-25 11:37:51 -0500

    trying to commit

--- Online-Store/src/main/java/com/pluralsight/Main.java
+++ Online-Store/src/main/java/com/pluralsight/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         //Relative path for file
-        String filePath = "src/main/resources/inventory.csv";
-        //This is Izel
+        String filePath = "src/main/resources/products.csv";
+        //This is Izel commit
 
     }
 }
